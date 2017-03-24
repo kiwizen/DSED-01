6f520a588f3c41226051c094904e457ec454ceda
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-03-24 23:14:57 +1300

    Create a new Folder called TEST_PROJ under the folder, create a C# project called MovingBoxApp edited the conveyorbell.png

---
+++ INITIAL_DESIGN/TEST_PROJ/MovingBoxApp/MovingBoxApp/FormMovingBox.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace MovingBoxApp
+{
+    partial class FormMovingBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.panelPictureBox = new System.Windows.Forms.Panel();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picBoxConveyorBelt = new System.Windows.Forms.PictureBox();
+            this.panelPictureBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxConveyorBelt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimer.Location = new System.Drawing.Point(51, 15);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(114, 42);
+            this.labelTimer.TabIndex = 2;
+            this.labelTimer.Text = "label1";
+            this.labelTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelPictureBox
+            // 
+            this.panelPictureBox.Controls.Add(this.pictureBox6);
+            this.panelPictureBox.Controls.Add(this.pictureBox5);
+            this.panelPictureBox.Controls.Add(this.pictureBox4);
+            this.panelPictureBox.Controls.Add(this.pictureBox3);
+            this.panelPictureBox.Controls.Add(this.pictureBox2);
+            this.panelPictureBox.Controls.Add(this.pictureBox1);
+            this.panelPictureBox.Location = new System.Drawing.Point(55, 80);
+            this.panelPictureBox.Name = "panelPictureBox";
+            this.panelPictureBox.Size = new System.Drawing.Size(556, 90);
+            this.panelPictureBox.TabIndex = 3;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::MovingBoxApp.Properties.Resources.box;
+            this.pictureBox6.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::MovingBoxApp.Properties.Resources.box;
+            this.pictureBox5.Location = new System.Drawing.Point(90, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::MovingBoxApp.Properties.Resources.box;
+            this.pictureBox4.Location = new System.Drawing.Point(180, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::MovingBoxApp.Properties.Resources.box;
+            this.pictureBox3.Location = new System.Drawing.Point(270, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MovingBoxApp.Properties.Resources.box;
+            this.pictureBox2.Location = new System.Drawing.Point(360, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MovingBoxApp.Properties.Resources.box;
+            this.pictureBox1.Location = new System.Drawing.Point(450, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // picBoxConveyorBelt
+            // 
+            this.picBoxConveyorBelt.Image = global::MovingBoxApp.Properties.Resources.conveyorbell01;
+            this.picBoxConveyorBelt.Location = new System.Drawing.Point(12, 171);
+            this.picBoxConveyorBelt.Name = "picBoxConveyorBelt";
+            this.picBoxConveyorBelt.Size = new System.Drawing.Size(717, 110);
+            this.picBoxConveyorBelt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxConveyorBelt.TabIndex = 0;
+            this.picBoxConveyorBelt.TabStop = false;
+            // 
+            // FormMovingBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 311);
+            this.Controls.Add(this.panelPictureBox);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.picBoxConveyorBelt);
+            this.Name = "FormMovingBox";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Text = "Moving Box";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMovingBox_FormClosing);
+            this.panelPictureBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxConveyorBelt)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxConveyorBelt;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Panel panelPictureBox;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox6;
+    }
+}
+
---
+++ INITIAL_DESIGN/TEST_PROJ/MovingBoxApp/MovingBoxApp/FormMovingBox.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingBoxApp
+{
+    public partial class FormMovingBox : Form
+    {
+        const int INITIAL_X = 55;
+        const int INITIAL_Y = 80;
+        const int STEP = 15;
+
+        private int count = 0;
+
+        public FormMovingBox()
+        {
+            InitializeComponent();
+
+
+
+            labelTimer.Text = "1";
+            // set timer interval to 5 second
+            timer1.Interval = 1000;
+            // add a event hander for timer class
+            timer1.Tick += new System.EventHandler(Tick);
+            // start the timer
+            timer1.Start();
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            int getInt(string text) { return int.Parse(text); }
+
+            int i = getInt(labelTimer.Text);
+            i = i % 5 + 1;
+
+            if (count / 6 == 6) panelPictureBox.Visible = false;
+
+            if(++count%6==0)
+            {
+                if(count/6==6)
+                {
+                    List <string> listPicBoxName = new List<string> { "pictureBox1","pictureBox2","pictureBox3",
+                                                                "pictureBox4","pictureBox5","pictureBox6" };
+
+                    foreach (Control screenControl in panelPictureBox.Controls)
+                        if (screenControl is PictureBox && listPicBoxName.Contains(screenControl.Name))
+                            screenControl.Visible = true;
+
+                    count = 0;
+                }
+                else
+                {
+                    int index = count / 6 ;
+                    foreach (Control screenControl in panelPictureBox.Controls)
+                        if (screenControl is PictureBox && 
+                            screenControl.Name == "pictureBox" + index)
+                            screenControl.Visible = false;
+                }
+            }
+
+            if (count % 2 == 0)
+                picBoxConveyorBelt.Image = Properties.Resources.conveyorbell01;
+            else
+                picBoxConveyorBelt.Image = Properties.Resources.conveyorbell02;
+
+            labelTimer.Text = "" + i;
+
+            panelPictureBox.Visible = true;
+            panelPictureBox.Location = new Point(INITIAL_X + STEP * (count), INITIAL_Y);
+            panelPictureBox.Refresh();
+        }
+        private void FormMovingBox_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // stop the timer
+            timer1.Stop();
+        }
+    }
+}
