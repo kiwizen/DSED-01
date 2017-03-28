b451f95fe77de67c4496090116b529efa90b153f
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-03-28 15:58:43 +1300

    Create a new Project solution called OpenBox

---
+++ INITIAL_DESIGN/TEST_PROJ/OpenBoxApp/OpenBoxApp/BoxOpeningForm.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace OpenBoxApp
+{
+    partial class BoxOpeningForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BoxOpeningForm));
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.panelPictureBox = new System.Windows.Forms.Panel();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picBoxToBeChecked = new System.Windows.Forms.PictureBox();
+            this.picBoxConveyorBelt = new System.Windows.Forms.PictureBox();
+            this.picBoxTable = new System.Windows.Forms.PictureBox();
+            this.panelPictureBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxToBeChecked)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxConveyorBelt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimer.Location = new System.Drawing.Point(485, 13);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(141, 27);
+            this.labelTimer.TabIndex = 2;
+            this.labelTimer.Text = "labelTimer";
+            this.labelTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelPictureBox
+            // 
+            this.panelPictureBox.Controls.Add(this.pictureBox6);
+            this.panelPictureBox.Controls.Add(this.pictureBox5);
+            this.panelPictureBox.Controls.Add(this.pictureBox4);
+            this.panelPictureBox.Controls.Add(this.pictureBox3);
+            this.panelPictureBox.Controls.Add(this.pictureBox2);
+            this.panelPictureBox.Controls.Add(this.pictureBox1);
+            this.panelPictureBox.Location = new System.Drawing.Point(45, 94);
+            this.panelPictureBox.Name = "panelPictureBox";
+            this.panelPictureBox.Size = new System.Drawing.Size(533, 75);
+            this.panelPictureBox.TabIndex = 3;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(75, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(150, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(225, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(300, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(375, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Old English Text MT", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(67, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(398, 59);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Bomb  in  the  Box";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picBoxToBeChecked
+            // 
+            this.picBoxToBeChecked.Image = global::OpenBoxApp.Properties.Resources.box_on_table;
+            this.picBoxToBeChecked.Location = new System.Drawing.Point(640, 106);
+            this.picBoxToBeChecked.Name = "picBoxToBeChecked";
+            this.picBoxToBeChecked.Size = new System.Drawing.Size(75, 75);
+            this.picBoxToBeChecked.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxToBeChecked.TabIndex = 5;
+            this.picBoxToBeChecked.TabStop = false;
+            this.picBoxToBeChecked.Visible = false;
+            // 
+            // picBoxConveyorBelt
+            // 
+            this.picBoxConveyorBelt.Image = global::OpenBoxApp.Properties.Resources.conveyorbelt01;
+            this.picBoxConveyorBelt.Location = new System.Drawing.Point(0, 168);
+            this.picBoxConveyorBelt.Name = "picBoxConveyorBelt";
+            this.picBoxConveyorBelt.Size = new System.Drawing.Size(607, 94);
+            this.picBoxConveyorBelt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxConveyorBelt.TabIndex = 0;
+            this.picBoxConveyorBelt.TabStop = false;
+            // 
+            // picBoxTable
+            // 
+            this.picBoxTable.Image = ((System.Drawing.Image)(resources.GetObject("picBoxTable.Image")));
+            this.picBoxTable.Location = new System.Drawing.Point(584, 144);
+            this.picBoxTable.Name = "picBoxTable";
+            this.picBoxTable.Size = new System.Drawing.Size(190, 140);
+            this.picBoxTable.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxTable.TabIndex = 4;
+            this.picBoxTable.TabStop = false;
+            // 
+            // BoxOpeningForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(864, 352);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picBoxToBeChecked);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.picBoxConveyorBelt);
+            this.Controls.Add(this.picBoxTable);
+            this.Controls.Add(this.panelPictureBox);
+            this.Name = "BoxOpeningForm";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Text = "Opening a Box";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.BoxOpeningForm_FormClosing);
+            this.panelPictureBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxToBeChecked)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxConveyorBelt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxConveyorBelt;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Panel panelPictureBox;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox picBoxTable;
+        private System.Windows.Forms.PictureBox picBoxToBeChecked;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ INITIAL_DESIGN/TEST_PROJ/OpenBoxApp/OpenBoxApp/BoxOpeningForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenBoxApp
+{
+    
+    public partial class BoxOpeningForm : Form
+    {
+        private PanelTimer myPanelTimer;
+
+        public BoxOpeningForm()
+        {
+            InitializeComponent();
+            myPanelTimer = new PanelTimer();
+            myPanelTimer.PanelControl = panelPictureBox;
+            myPanelTimer.start();
+        }
+
+        private void BoxOpeningForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myPanelTimer.stop();
+        }
+    }
+
+    public class PanelTimer 
+    {
+        internal int INITIAL_X { get => PanelControl.Location.X; }
+        internal int INITIAL_Y { get => PanelControl.Location.Y; }
+
+        private List<string> listPicBoxName = new List<string> { "pictureBox1","pictureBox2","pictureBox3",
+                                                                "pictureBox4","pictureBox5","pictureBox6" };
+        const int STEP = 15;
+
+        private int origianl_x, original_y;
+        private int count;
+        private int picBoxIndex;
+
+        private Timer myTimer;
+        private Panel myPanel;
+
+        public Panel PanelControl
+        {
+            set {
+                myPanel = value;
+                origianl_x = myPanel.Location.X;
+                original_y = myPanel.Location.Y;
+            }
+            get => myPanel;
+        }
+
+        public PanelTimer()
+        {
+            count = 0;
+            myTimer = new Timer();
+            // set timer interval to 5 second
+            myTimer.Interval = 1000;
+            // add a event hander for timer class
+            myTimer.Tick += new System.EventHandler(Tick);
+
+            picBoxIndex = 1;
+
+        }
+
+        public void start()
+        {
+            myTimer.Start();
+        }
+
+
+        private void Tick(object sender, EventArgs e)
+        {
+            if (++count % 5 == 0)  // Every 5 seconds, do something
+            {
+                myTimer.Stop();
+
+                if (++picBoxIndex > 6)
+                {
+                    count = 0;
+                    picBoxIndex = 1;
+                    foreach (Control screenControl in PanelControl.Controls)
+                        if (screenControl is PictureBox && listPicBoxName.Contains(screenControl.Name))
+                            screenControl.Visible = true;
+                }
+
+            }
+
+            if(count==0)
+                PanelControl.Location = new Point(origianl_x, original_y);
+            else
+                PanelControl.Location = new Point(INITIAL_X + STEP, INITIAL_Y);
+
+            PanelControl.Visible = true;
+            
+            PanelControl.Refresh();
+
+        }
+
+        public void MoveBox()
+        {
+            int index = count / 5;
+            foreach (Control screenControl in PanelControl.Controls)
+                if (screenControl is PictureBox &&
+                    screenControl.Name == "pictureBox" + index)
+                    screenControl.Visible = false;
+        }
+
+        private void myTick(object sender, EventArgs e)
+        {
+            if (++count % 5 == 0)  // Every 5 seconds, do something
+            {
+                //int index = count / 6;
+                foreach (Control screenControl in PanelControl.Controls)
+                    if (screenControl is PictureBox &&
+                        screenControl.Name == "pictureBox" + picBoxIndex)
+                        screenControl.Visible = false;
+                //myTimer.Stop();
+
+                if (++picBoxIndex > 6)
+                {
+                    count = 0;
+                    picBoxIndex = 1;
+                    foreach (Control screenControl in PanelControl.Controls)
+                        if (screenControl is PictureBox && listPicBoxName.Contains(screenControl.Name))
+                            screenControl.Visible = true;
+                }
+
+            }
+
+            if (count == 0)
+                PanelControl.Location = new Point(origianl_x, original_y);
+            else
+                PanelControl.Location = new Point(INITIAL_X + STEP, INITIAL_Y);
+
+            PanelControl.Visible = true;
+
+            PanelControl.Refresh();
+        }
+
+        internal void stop()
+        {
+            myTimer.Stop();
+        }
+    }
+}
