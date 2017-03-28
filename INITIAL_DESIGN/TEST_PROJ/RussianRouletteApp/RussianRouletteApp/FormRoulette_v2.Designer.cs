e008e0c1e3e43d6b12b839df4850852747f624b4
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-03-28 19:45:05 +1300

    ammend the C# solution and also rename conveyorbelt bitmap

--- INITIAL_DESIGN/TEST_PROJ/RussianRouletteApp/RussianRouletteApp/FormRoulette_v2.Designer.cs
+++ INITIAL_DESIGN/TEST_PROJ/RussianRouletteApp/RussianRouletteApp/FormRoulette_v2.Designer.cs
@@ -30,75 +30,76 @@
         {
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.labelNum = new System.Windows.Forms.Label();
-            this.labelBullet = new System.Windows.Forms.Label();
+            this.labelCurrentID = new System.Windows.Forms.Label();
+            this.labelBombLocation = new System.Windows.Forms.Label();
             this.labelWin = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.labelLoss = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.labelChance = new System.Windows.Forms.Label();
+            this.labelNoOfRobot = new System.Windows.Forms.Label();
             this.labelGame = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.buttonNew = new System.Windows.Forms.Button();
-            this.buttonFire = new System.Windows.Forms.Button();
+            this.buttonOpenBox = new System.Windows.Forms.Button();
             this.buttonSpin = new System.Windows.Forms.Button();
-            this.buttonSkip = new System.Windows.Forms.Button();
+            this.buttonUseRobot = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(54, 159);
+            this.label2.Location = new System.Drawing.Point(53, 164);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(134, 29);
+            this.label2.Size = new System.Drawing.Size(142, 27);
             this.label2.TabIndex = 11;
-            this.label2.Text = "You start fire at :";
+            this.label2.Text = "Current Box ID :";
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(46, 118);
+            this.label1.Location = new System.Drawing.Point(98, 128);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 29);
+            this.label1.Size = new System.Drawing.Size(97, 27);
             this.label1.TabIndex = 10;
-            this.label1.Text = "Bullet Loaded at :";
+            this.label1.Text = "Bomb is at :";
             // 
-            // labelNum
+            // labelCurrentID
             // 
-            this.labelNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.labelNum.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelNum.Location = new System.Drawing.Point(194, 157);
-            this.labelNum.Name = "labelNum";
-            this.labelNum.Size = new System.Drawing.Size(74, 29);
-            this.labelNum.TabIndex = 9;
-            this.labelNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelCurrentID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelCurrentID.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCurrentID.Location = new System.Drawing.Point(201, 163);
+            this.labelCurrentID.Name = "labelCurrentID";
+            this.labelCurrentID.Size = new System.Drawing.Size(74, 27);
+            this.labelCurrentID.TabIndex = 9;
+            this.labelCurrentID.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // labelBullet
+            // labelBombLocation
             // 
-            this.labelBullet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.labelBullet.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelBullet.Location = new System.Drawing.Point(194, 118);
-            this.labelBullet.Name = "labelBullet";
-            this.labelBullet.Size = new System.Drawing.Size(74, 29);
-            this.labelBullet.TabIndex = 8;
-            this.labelBullet.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelBombLocation.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelBombLocation.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBombLocation.Location = new System.Drawing.Point(201, 127);
+            this.labelBombLocation.Name = "labelBombLocation";
+            this.labelBombLocation.Size = new System.Drawing.Size(74, 27);
+            this.labelBombLocation.TabIndex = 8;
+            this.labelBombLocation.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // labelWin
             // 
             this.labelWin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.labelWin.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelWin.Location = new System.Drawing.Point(114, 59);
+            this.labelWin.Location = new System.Drawing.Point(121, 72);
             this.labelWin.Name = "labelWin";
-            this.labelWin.Size = new System.Drawing.Size(74, 29);
+            this.labelWin.Size = new System.Drawing.Size(74, 27);
             this.labelWin.TabIndex = 21;
             this.labelWin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label5
             // 
             this.label5.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(109, 30);
+            this.label5.Location = new System.Drawing.Point(116, 46);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 29);
+            this.label5.Size = new System.Drawing.Size(67, 27);
             this.label5.TabIndex = 20;
             this.label5.Text = "Win :";
             // 
@@ -106,46 +107,46 @@
             // 
             this.labelLoss.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.labelLoss.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelLoss.Location = new System.Drawing.Point(199, 59);
+            this.labelLoss.Location = new System.Drawing.Point(206, 72);
             this.labelLoss.Name = "labelLoss";
-            this.labelLoss.Size = new System.Drawing.Size(74, 29);
+            this.labelLoss.Size = new System.Drawing.Size(74, 27);
             this.labelLoss.TabIndex = 19;
             this.labelLoss.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(194, 30);
+            this.label4.Location = new System.Drawing.Point(201, 46);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 29);
+            this.label4.Size = new System.Drawing.Size(67, 27);
             this.label4.TabIndex = 18;
             this.label4.Text = "Loss :";
             // 
-            // labelChance
+            // labelNoOfRobot
             // 
-            this.labelChance.Font = new System.Drawing.Font("Papyrus", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelChance.Location = new System.Drawing.Point(32, 207);
-            this.labelChance.Name = "labelChance";
-            this.labelChance.Size = new System.Drawing.Size(303, 36);
-            this.labelChance.TabIndex = 17;
-            this.labelChance.Text = "You have on ? chance left.";
+            this.labelNoOfRobot.Font = new System.Drawing.Font("Papyrus", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNoOfRobot.Location = new System.Drawing.Point(85, 217);
+            this.labelNoOfRobot.Name = "labelNoOfRobot";
+            this.labelNoOfRobot.Size = new System.Drawing.Size(149, 83);
+            this.labelNoOfRobot.TabIndex = 17;
+            this.labelNoOfRobot.Text = "You have ? robot left.";
             // 
             // labelGame
             // 
             this.labelGame.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.labelGame.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelGame.Location = new System.Drawing.Point(27, 59);
+            this.labelGame.Location = new System.Drawing.Point(34, 72);
             this.labelGame.Name = "labelGame";
-            this.labelGame.Size = new System.Drawing.Size(74, 29);
+            this.labelGame.Size = new System.Drawing.Size(74, 27);
             this.labelGame.TabIndex = 16;
             this.labelGame.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(34, 30);
+            this.label3.Location = new System.Drawing.Point(41, 46);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 29);
+            this.label3.Size = new System.Drawing.Size(67, 27);
             this.label3.TabIndex = 15;
             this.label3.Text = "Game :";
             // 
@@ -153,7 +154,7 @@
             // 
             this.buttonNew.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonNew.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.buttonNew.Location = new System.Drawing.Point(313, 11);
+            this.buttonNew.Location = new System.Drawing.Point(313, 55);
             this.buttonNew.Name = "buttonNew";
             this.buttonNew.Size = new System.Drawing.Size(140, 62);
             this.buttonNew.TabIndex = 25;
@@ -161,60 +162,72 @@
             this.buttonNew.UseVisualStyleBackColor = true;
             this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
             // 
-            // buttonFire
+            // buttonOpenBox
             // 
-            this.buttonFire.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonFire.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.buttonFire.Location = new System.Drawing.Point(313, 159);
-            this.buttonFire.Name = "buttonFire";
-            this.buttonFire.Size = new System.Drawing.Size(140, 75);
-            this.buttonFire.TabIndex = 24;
-            this.buttonFire.Text = "Fire";
-            this.buttonFire.UseVisualStyleBackColor = true;
-            this.buttonFire.Click += new System.EventHandler(this.buttonFire_Click);
+            this.buttonOpenBox.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOpenBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.buttonOpenBox.Location = new System.Drawing.Point(313, 128);
+            this.buttonOpenBox.Name = "buttonOpenBox";
+            this.buttonOpenBox.Size = new System.Drawing.Size(140, 69);
+            this.buttonOpenBox.TabIndex = 24;
+            this.buttonOpenBox.Text = "Open the Box";
+            this.buttonOpenBox.UseVisualStyleBackColor = true;
+            this.buttonOpenBox.Click += new System.EventHandler(this.buttonOpenBox_Click);
             // 
             // buttonSpin
             // 
             this.buttonSpin.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonSpin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.buttonSpin.Location = new System.Drawing.Point(313, 82);
+            this.buttonSpin.Location = new System.Drawing.Point(313, 55);
             this.buttonSpin.Name = "buttonSpin";
-            this.buttonSpin.Size = new System.Drawing.Size(140, 65);
+            this.buttonSpin.Size = new System.Drawing.Size(140, 60);
             this.buttonSpin.TabIndex = 23;
             this.buttonSpin.Text = "Spin Chambers";
             this.buttonSpin.UseVisualStyleBackColor = true;
             // 
-            // buttonSkip
+            // buttonUseRobot
             // 
-            this.buttonSkip.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonSkip.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.buttonSkip.Location = new System.Drawing.Point(12, 257);
-            this.buttonSkip.Name = "buttonSkip";
-            this.buttonSkip.Size = new System.Drawing.Size(309, 46);
-            this.buttonSkip.TabIndex = 22;
-            this.buttonSkip.Text = "Take a chance and skip the number.";
-            this.buttonSkip.UseVisualStyleBackColor = true;
+            this.buttonUseRobot.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUseRobot.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.buttonUseRobot.Location = new System.Drawing.Point(258, 217);
+            this.buttonUseRobot.Name = "buttonUseRobot";
+            this.buttonUseRobot.Size = new System.Drawing.Size(195, 71);
+            this.buttonUseRobot.TabIndex = 22;
+            this.buttonUseRobot.Text = "Use Robot to open the box";
+            this.buttonUseRobot.UseVisualStyleBackColor = true;
+            this.buttonUseRobot.Click += new System.EventHandler(this.buttonUseRobot_Click);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Papyrus", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(27, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(303, 33);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Bomb in the Box !!!";
             // 
             // FormRoulette_v2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(474, 325);
+            this.ClientSize = new System.Drawing.Size(474, 300);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.buttonNew);
-            this.Controls.Add(this.buttonFire);
-            this.Controls.Add(this.buttonSpin);
-            this.Controls.Add(this.buttonSkip);
+            this.Controls.Add(this.buttonOpenBox);
+            this.Controls.Add(this.buttonUseRobot);
             this.Controls.Add(this.labelWin);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.labelLoss);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.labelChance);
+            this.Controls.Add(this.labelNoOfRobot);
             this.Controls.Add(this.labelGame);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.labelNum);
-            this.Controls.Add(this.labelBullet);
+            this.Controls.Add(this.labelCurrentID);
+            this.Controls.Add(this.labelBombLocation);
+            this.Controls.Add(this.buttonSpin);
             this.Name = "FormRoulette_v2";
             this.Text = "Russian Roulette version 2";
             this.ResumeLayout(false);
@@ -225,18 +238,19 @@
 
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label labelNum;
-        private System.Windows.Forms.Label labelBullet;
+        private System.Windows.Forms.Label labelCurrentID;
+        private System.Windows.Forms.Label labelBombLocation;
         private System.Windows.Forms.Label labelWin;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label labelLoss;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label labelChance;
+        private System.Windows.Forms.Label labelNoOfRobot;
         private System.Windows.Forms.Label labelGame;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button buttonNew;
-        private System.Windows.Forms.Button buttonFire;
+        private System.Windows.Forms.Button buttonOpenBox;
         private System.Windows.Forms.Button buttonSpin;
-        private System.Windows.Forms.Button buttonSkip;
+        private System.Windows.Forms.Button buttonUseRobot;
+        private System.Windows.Forms.Label label6;
     }
 }
--- INITIAL_DESIGN/TEST_PROJ/RussianRouletteApp/RussianRouletteApp/FormRoulette_v2.cs
+++ INITIAL_DESIGN/TEST_PROJ/RussianRouletteApp/RussianRouletteApp/FormRoulette_v2.cs
@@ -12,19 +12,23 @@ namespace RussianRouletteApp
 {
     public partial class FormRoulette_v2 : Form
     {
-        private ClassRoulette myClassRoulette;
+        private BombInBoxGame myClassRoulette;
         public FormRoulette_v2()
         {
-            myClassRoulette = new ClassRoulette();
-            myClassRoulette.NewGame();
+            myClassRoulette = new BombInBoxGame();
+            //myClassRoulette.NewGame();
             InitializeComponent();
             refreshScreen();
         }
 
-        private void refreshScreen ()
+        private void refreshScreen()
         {
-            labelBullet.Text = myClassRoulette.Value.ToString();
-            labelNum.Text = myClassRoulette.Counter.ToString();
+            labelBombLocation.Text = myClassRoulette.SecretBoxID.ToString();
+            labelCurrentID.Text = myClassRoulette.CurrentBoxID.ToString();
+            labelGame.Text = myClassRoulette.TotalGamePlayed.ToString();
+            labelWin.Text = myClassRoulette.Win.ToString();
+            labelLoss.Text = myClassRoulette.Lose.ToString();
+            labelNoOfRobot.Text = "You have " + myClassRoulette.RobotCount + " robot left.";
         }
 
         private void buttonNew_Click(object sender, EventArgs e)
@@ -33,20 +37,48 @@ namespace RussianRouletteApp
             refreshScreen();
         }
 
-        private void buttonFire_Click(object sender, EventArgs e)
-        {
-            if (myClassRoulette.isShoot())
-            {
-                MessageBox.Show("You have been shot!!!\nPlease try again");
 
+        private void buttonOpenBox_Click(object sender, EventArgs e)
+        {
+            if (myClassRoulette.isBomb())
+            {
+                MessageBox.Show("You have just detonated a bomb in the Box!!!\nYou Lose!!!\nPlease try again");
+                myClassRoulette.YouLose();
                 myClassRoulette.NewGame();
             }
             else
             {
                 myClassRoulette.Next();
+                if (myClassRoulette.CurrentBoxID == myClassRoulette.NoOfBox)
+                {
+                    MessageBox.Show($"You have open all {myClassRoulette.NoOfBox-1} box and found the bomb in the 6th box.\nWell Done.\nYou have won the game!!!");
+                    myClassRoulette.YouWon();
+                    myClassRoulette.NewGame();
+                }
             }
             refreshScreen();
-            
+
+        }
+        private void buttonUseRobot_Click(object sender, EventArgs e)
+        {
+            if (myClassRoulette.isBomb())
+            {
+                MessageBox.Show("You have used a robot to defuse a bomb.\nWell Done.\nYou have won the game!!!");
+                myClassRoulette.YouWon();
+                myClassRoulette.NewGame();
+
+            }
+            else
+            {
+                myClassRoulette.RobotCount--;
+                if(myClassRoulette.RobotCount==0)
+                {
+                    MessageBox.Show("Sorry You used up all the robot and still could not find the bomb.\nYou lose!!!\nPlease try again");
+                    myClassRoulette.YouLose();
+                    myClassRoulette.NewGame();
+                }
+            }
+            refreshScreen();
         }
     }
 }
