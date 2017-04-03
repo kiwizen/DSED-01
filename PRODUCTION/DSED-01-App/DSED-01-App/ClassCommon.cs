a11c0e17b4c168f883496c172180d22b7a1448b1
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-04-04 11:50:51 +1200

    Create a Folder for Finishing App

---
+++ PRODUCTION/DSED-01-App/DSED-01-App/ClassCommon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSED_01_App
+{
+    class CommonControlClass
+    {
+        private static int _index = 0;
+        public CommonControlClass()
+        {
+        }
+    }
+}
